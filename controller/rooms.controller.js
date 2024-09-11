553b2c3f772c36ecee64bfa25e31b32209d9c370
bitsen03 <bitsen0359@gmail.com>
2024-09-11 21:07:04 +0300

    ypdate rooms.controller

--- controller/rooms.controller.js
+++ controller/rooms.controller.js
@@ -2,8 +2,8 @@ import db from "../db.js";
 
 class RoomController {
     async createRoom (req, res) {
-        const {number, category, number_of_seats, status_free} = req.body;
-        const newHotel = await db.query(`INSERT INTO rooms (number, category, number_of_seats, status_free) values ($1, $2, $3, $4) RETURNING *`, [number, category, number_of_seats, status_free]);
+        const {number, category, status_free} = req.body;
+        const newHotel = await db.query(`INSERT INTO rooms (number, category, status_free) values ($1, $2, $3, ) RETURNING *`, [number, category, status_free]);
         res.json(newHotel.rows[0]);
     }
     async getFreeRoom (req, res) {
