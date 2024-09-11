99578ee5b03f9a6a3f94e5a6d25261d7cff04482
bitsen03 <bitsen0359@gmail.com>
2024-09-11 20:12:30 +0300

    qwe

--- router/fond.routes.js
+++ router/fond.routes.js
@@ -1,10 +1,10 @@
-import { Router } from "express";
-import fondController from "../controller/fond.controller";
+// import { Router } from "express";
+// import fondController from "../controller/fond.controller";
 
-const router = new Router();
+// const router = new Router();
 
-router.post('/hotel', fondController.createFond);
-router.get('/hotel', fondController.getFreeHotel);
-router.get('/hotel/:id', fondController.reverseStatus)
+// router.post('/hotel', fondController.createFond);
+// router.get('/hotel', fondController.getFreeHotel);
+// router.get('/hotel/:id', fondController.reverseStatus)
 
-export default router;
+// export default router;
